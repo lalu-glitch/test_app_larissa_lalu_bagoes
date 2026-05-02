ec713aac577383c7df45d7859ea4377b02874dfb
pc-kecil <pckecil@desktoppc.com>
2026-05-02 14:45:58 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/data/presentation/screens/product_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ProductListScreen(),
+    );
+  }
+}
