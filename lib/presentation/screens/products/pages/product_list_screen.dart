aae3af003bae7ba31f946a09731b5b65c2e372eb
crowdfreak <lalubagoes2@gmail.com>
2026-05-05 19:05:41 +0800

    feat: halama product list

--- lib/presentation/screens/products/pages/product_list_screen.dart
+++ lib/presentation/screens/products/pages/product_list_screen.dart
@@ -10,37 +10,59 @@ class ProductListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void refresh() {
+      context.read<ProductsCubit>().getProducts();
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('Product List')),
-      body: Container(
-        color: Colors.amber,
-        child: Padding(
-          padding: .symmetric(horizontal: 8, vertical: 14),
-          child: BlocBuilder<ProductsCubit, ProductsState>(
-            builder: (context, state) {
-              if (state is ProductsInitial) {
-                context.read<ProductsCubit>().getProducts();
-              }
-              if (state is ProductsLoading) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              if (state is ProductsLoaded) {
-                return ListView.builder(
-                  itemCount: state.products.length,
-                  itemBuilder: (context, index) {
-                    Product dataProduct = state.products[index];
-                    return ProductCard(data: dataProduct);
-                  },
-                );
-              }
-              if (state is ProductsEmpty) {
-                return const Center(child: Text('Data tidak ada'));
-              }
-              if (state is ProductsError) {
-                return Center(child: Text(state.message));
-              }
-              return const SizedBox.shrink();
-            },
+      body: RefreshIndicator(
+        onRefresh: () async => refresh(),
+        child: Container(
+          color: Colors.amber,
+          child: Padding(
+            padding: .symmetric(horizontal: 8, vertical: 14),
+            child: BlocBuilder<ProductsCubit, ProductsState>(
+              builder: (context, state) {
+                if (state is ProductsInitial) {
+                  context.read<ProductsCubit>().getProducts();
+                }
+                if (state is ProductsLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (state is ProductsLoaded) {
+                  return ListView.builder(
+                    itemCount: state.products.length,
+                    itemBuilder: (context, index) {
+                      Product dataProduct = state.products[index];
+                      return ProductCard(data: dataProduct);
+                    },
+                  );
+                }
+                if (state is ProductsEmpty) {
+                  return const Center(child: Text('Data tidak ada'));
+                }
+                if (state is ProductsError) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: .center,
+                      children: [
+                        Icon(Icons.error, color: Colors.red, weight: 150),
+                        Text(
+                          state.message,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async => refresh(),
+                          child: Text('refresh'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
           ),
         ),
       ),
